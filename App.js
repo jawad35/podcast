13d7c83e17559b8fb5f89e9d1f8dbecc2dd70070
jawad35 <jaqimughal@gmail.com>
2024-01-09 20:38:29 +0500

    Batch8

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 // import Routes from './src/navigations/routes';
 import { Provider } from 'react-redux';
 import PodcastStore from './src/redux/store/PodCastStore';
-import { StripeProvider } from '@stripe/stripe-react-native';
 export default function App() {
   return (
     <NavigationContainer>
       {/* <Routes/> */}
       <Provider store={PodcastStore}>
-  
+    
         
           <AppNavigation />
       </Provider>
