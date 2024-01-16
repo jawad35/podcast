b783d044da2f2b4cf13c90caebfa7136cc663097
jawad35 <jaqimughal@gmail.com>
2024-01-16 21:01:26 +0500

    batch10

--- src/screens/podcaster/TrendingPosts.js
+++ src/screens/podcaster/TrendingPosts.js
@@ -7,15 +7,12 @@ import { scale } from 'react-native-size-matters'
 import { useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux'
 import { defaultProfile } from '../../utils/Constants'
-import { StripeProvider } from '@stripe/stripe-react-native';
-
-
 export default function TrendingPosts() {
   const [TrendingPodcasts, setTrendingPodcast] = useState([])
   const podcastData = useSelector(state => state.userData)
 
   const navigation = useNavigation()
-  
+
   const GetTrendingPodcast = async () => {
     const response = await ApiUrl.get(`/api/user/trendings`);
     if (response.data.success) {
@@ -26,41 +23,34 @@ export default function TrendingPosts() {
     GetTrendingPodcast()
   }, [podcastData])
   return (
-    <StripeProvider
-      publishableKey="pk_test_51MslBhAeOlKaLrSISkg3FHP8WAkD6kMSK4sthpUs5Bha1H1u4Cs29X7EucKZXjRITKypWk7cY0SpQpvxffR0OiAF00c0q8fYOi"
-      merchantIdentifier="merchant.com.podcast" // required for Apple Pay
-    >
-      // Your app code here
-      <SafeAreaView className='bg-black flex-1'>
+    <SafeAreaView className='bg-black flex-1'>
       <HeaderTitle title={'Podcast Trendings'} />
       {
-        TrendingPodcasts.length !== 0 ?  <View className='p-2'>
-        <ScrollView  contentContainerStyle={{ flexGrow: 1, paddingBottom: scale(180) }}>
-          {
-            TrendingPodcasts?.map((item, index) => {
-              return <TouchableOpacity key={index} onPress={() => navigation.navigate("Profile", { userid: item?._id })} className='relative'>
-                <Text className='absolute z-10 bg-white_color text-black' style={{ margin: scale(10), paddingHorizontal: scale(10), paddingVertical: scale(5), borderRadius: scale(6), bottom: scale(10) }}>
-                  {
-                    item?.fullname?.length > 20 ? CapitalizeString(item.fullname).substring(0, 20) + '...' : CapitalizeString(item.fullname)
-                  }
-                </Text>
-                <Image key={index} source={{ uri: item.avatar ? `${item.avatar}` : defaultProfile }}
-                  style={{ height: scale(250), width: '100%' }}
-                  resizeMode='cover'
-                  className='rounded-lg my-2'
-                />
-              </TouchableOpacity>
-            })
-          }
+        TrendingPodcasts.length !== 0 ? <View className='p-2'>
+          <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: scale(180) }}>
+            {
+              TrendingPodcasts?.map((item, index) => {
+                return <TouchableOpacity key={index} onPress={() => navigation.navigate("Profile", { userid: item?._id })} className='relative'>
+                  <Text className='absolute z-10 bg-white_color text-black' style={{ margin: scale(10), paddingHorizontal: scale(10), paddingVertical: scale(5), borderRadius: scale(6), bottom: scale(10) }}>
+                    {
+                      item?.fullname?.length > 20 ? CapitalizeString(item.fullname).substring(0, 20) + '...' : CapitalizeString(item.fullname)
+                    }
+                  </Text>
+                  <Image key={index} source={{ uri: item.avatar ? `${item.avatar}` : defaultProfile }}
+                    style={{ height: scale(250), width: '100%' }}
+                    resizeMode='cover'
+                    className='rounded-lg my-2'
+                  />
+                </TouchableOpacity>
+              })
+            }
 
-        </ScrollView>
-      </View> :  <View className='flex-1 justify-center items-center'>
-      <Text className='text-white_color'>No Podcast</Text>
-      </View>
+          </ScrollView>
+        </View> : <View className='flex-1 justify-center items-center'>
+          <Text className='text-white_color'>No Podcast</Text>
+        </View>
       }
 
     </SafeAreaView>
-    </StripeProvider>
-    
   )
 }
