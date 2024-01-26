21018b667180b5086e052a63c523e69dee5f6cea
jawad35 <jaqimughal@gmail.com>
2024-01-26 16:35:28 +0500

    Batch12

--- ios/PodcastTonightTests/PodcastTests.m
+++ ios/PodcastTonightTests/PodcastTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PodcastTonightTests : XCTestCase
+@interface podcasttonightTests : XCTestCase
 
 @end
 
-@implementation PodcastTonightTests
+@implementation podcasttonightTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
